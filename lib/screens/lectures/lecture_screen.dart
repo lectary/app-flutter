a3338c6c8dad0e30b4704acd63631d49f0573559
Guenter <guenter.windsperger@gmx.at>
2020-05-18 12:19:41 +0200

    refactoring lecture screens
    - turning the main screen into a stateful widget
    - adding temp iconButton for switching empty/filled lecture screen
    - moving scaffold and its attributes till body to the LectureMainScreen
    - renaming class with a better suited name

--- lib/screens/lectures/lecture_screen.dart
+++ lib/screens/lectures/lecture_screen.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:lectary/i18n/localizations.dart';
 
 class LectureScreen extends StatelessWidget {
   LectureScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(AppLocalizations.of(context).appTitle),
-      ),
-      body: Center(
-          child: Text('Lecture Page - work in progress...'),
-      ),
+    return Center(
+      child: Text('Lecture Page - work in progress...'),
     );
   }
 }
--- lib/screens/lectures/main_screen.dart
+++ lib/screens/lectures/main_screen.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:lectary/screens/lectures/no_lectures_available_screen.dart';
+import 'package:lectary/i18n/localizations.dart';
+import 'package:lectary/screens/drawer/main_drawer.dart';
+import 'package:lectary/screens/lectures/lecture_not_available_screen.dart';
 import 'package:lectary/screens/lectures/lecture_screen.dart';
 
-class LectureMainScreen extends StatelessWidget {
-  LectureMainScreen({Key key}) : super(key: key);
+
+class LectureMainScreen extends StatefulWidget {
+  @override
+  _LectureMainScreenState createState() => _LectureMainScreenState();
+}
+
+class _LectureMainScreenState extends State<LectureMainScreen> {
 
   // ToDo connect with API
-  final bool lecturesAvailable = false;
+  bool lecturesAvailable = false;
 
   @override
   Widget build(BuildContext context) {
-    return lecturesAvailable ? LectureScreen() : NoLecturesAvailableScreen();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).appTitle),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.change_history),
+              onPressed: () {
+                setState(() {
+                  lecturesAvailable = lecturesAvailable ? false : true;
+                });
+              }
+          ),
+        ],
+      ),
+      drawer: MainDrawer(),
+      body: lecturesAvailable ? LectureScreen() : LectureNotAvailableScreen(),
+    );
   }
 }
