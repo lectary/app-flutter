6ce1170f1509a2a3449b02c6b9e09dafbf82c66c
Guenter <guenter.windsperger@gmx.at>
2020-05-22 23:21:39 +0200

    integrating custom lectary icon fonts to the project
    - adding dependency to asset folder
    - integrating icons into the lecture screen
    - slight adjustment/workaround for one rectangular icon for getting aligned properly (otherwise getting clipped)

--- lib/screens/lectures/lecture_screen.dart
+++ lib/screens/lectures/lecture_screen.dart
@@ -34,7 +34,7 @@ class _LectureScreenState extends State<LectureScreen> {
             children: <Widget>[
               _buildButton(
                   (slowModeOn ? ColorsLectary.yellow : ColorsLectary.darkblue),
-                  Icons.casino,
+                  IconData(0xe900, fontFamily: 'icomoon'),
                   35,
                   func: () => setState(() {
                     slowModeOn = slowModeOn ? false : true;
@@ -42,19 +42,21 @@ class _LectureScreenState extends State<LectureScreen> {
               ),
               _buildButton(
                   (autoModeOn ? ColorsLectary.orange : ColorsLectary.darkblue),
-                  Icons.casino,
+                  IconData(0xe901, fontFamily: 'icomoon'),
                   35,
+                  iconContainerWidth: 80, // extra container size for aligning rectangular icon correctly
                   func: () => setState(() {
-                    autoModeOn = autoModeOn ? false : true;
-                  })
+                  autoModeOn = autoModeOn ? false : true;
+                  }),
               ),
               _buildButton(
                   (repeatModeOn ? ColorsLectary.red : ColorsLectary.darkblue),
-                  Icons.casino,
+                  IconData(0xe902, fontFamily: 'icomoon'),
                   35,
                   func: () => setState(() {
                     repeatModeOn = repeatModeOn ? false : true;
-                  })),
+                  })
+              ),
             ],
           ),
         ),
@@ -73,13 +75,15 @@ class _LectureScreenState extends State<LectureScreen> {
                   })
               ),
               _buildButton(
-                  ColorsLectary.violett, Icons.casino, 70,
+                  ColorsLectary.violett, Icons.casino,
+                  70,
                   func: () => setState(() {
                     // TODO select vocable randomly
                   })
               ),
               _buildButton(
-                  ColorsLectary.lightblue, Icons.search, 70,
+                  ColorsLectary.lightblue, Icons.search,
+                  70,
                   func: () => setState(() {
                     // TODO search vocable
                   })),
@@ -90,14 +94,17 @@ class _LectureScreenState extends State<LectureScreen> {
     );
   }
 
-  Expanded _buildButton(color, icon, int size, {Function func=emptyFunction}) {
+  Expanded _buildButton(color, icon, int size, {int iconContainerWidth=0, Function func=emptyFunction}) {
     return Expanded(
       child: FlatButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(0))
         ),
         color: color,
-        child: Icon(icon, size: size.toDouble(), color: ColorsLectary.white),
+        child: Container( /// additional container for aligning rectangular icons correctly
+          width: iconContainerWidth == 0 ? size.toDouble() : iconContainerWidth.toDouble(),
+          child: Icon(icon, size: size.toDouble(), color: ColorsLectary.white),
+        ),
         onPressed: func,
       ),
     );
