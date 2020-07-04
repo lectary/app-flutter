ee433c017ec3d88fee19c21ccad82ee8e700bc05
Günter <e1302775@student.tuwien.ac.at>
2020-07-04 11:45:18 +0200

    adding player control overlay with progress indicator

--- lib/screens/lectures/widgets/lectary_video_player.dart
+++ lib/screens/lectures/widgets/lectary_video_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lectary/utils/colors.dart';
 import 'package:video_player/video_player.dart';
 
 class LectaryVideoPlayer extends StatefulWidget {
@@ -42,7 +43,7 @@ class _LectaryVideoPlayerState extends State<LectaryVideoPlayer> {
         if (snapshot.connectionState == ConnectionState.done) {
           return AspectRatio(
             aspectRatio: 4/3,
-            child: VideoPlayer(_controller)
+            child: _buildVideoPlayerWithOverlay(),
           );
         } else {
           return AspectRatio(
@@ -53,6 +54,43 @@ class _LectaryVideoPlayerState extends State<LectaryVideoPlayer> {
           );
         }
       }
+    );
+  }
+
+  Widget _buildVideoPlayerWithOverlay() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (_controller.value.isPlaying) {
+            _controller.pause();
+          } else {
+            _controller.play();
+          }
+        });
+      },
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          VideoPlayer(_controller),
+          Visibility(
+            visible: !_controller.value.isPlaying,
+            child: Container(
+              alignment: Alignment.center,
+              child: Opacity(
+                opacity: 0.3,
+                  child: Icon(Icons.play_circle_filled, size: 120, color: ColorsLectary.white,),
+              ),
+            ),
+          ),
+          VideoProgressIndicator(_controller, allowScrubbing: false,
+            colors: VideoProgressColors(
+              backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+              bufferedColor: Color.fromRGBO(0, 0, 0, 0),
+              playedColor: ColorsLectary.yellow
+            ),
+          )
+        ],
+      ),
     );
   }
 }
