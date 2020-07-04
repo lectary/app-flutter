5ffdabb1afa569f798748f1389979924d81964b2
Günter <e1302775@student.tuwien.ac.at>
2020-07-04 12:17:17 +0200

    adding carousel control overlay

--- lib/screens/lectures/lecture_screen.dart
+++ lib/screens/lectures/lecture_screen.dart
@@ -32,38 +32,43 @@ class _LectureScreenState extends State<LectureScreen> {
       children: <Widget>[
         Expanded(
           flex: 7,
-          child: CarouselSlider.builder(
-            carouselController: carouselController,
-            options: CarouselOptions(
-                height: (height / 10) * 7,
-                viewportFraction: 0.999999, // FIXME dirty hack to achieve pre-loading of previous/next page
-                autoPlay: false,
-                enlargeCenterPage: true
-            ),
-            itemCount: videoList.length,
-            itemBuilder: (BuildContext context, int itemIndex) =>
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                        child: vocableVisible
-                            ? Container(
-                          child: Text("Video #$itemIndex", style: TextStyle(color: ColorsLectary.white),), padding: EdgeInsets.all(10),)
-                            : IconButton(icon: Icon(Icons.visibility, color: ColorsLectary.green,), iconSize: 60,
-                          onPressed: () => setState(() {
-                            vocableVisible = vocableVisible ? false : true;
-                          }),
-                        )
-                    ),
-                    LectaryVideoPlayer(
-                      videoPath: videoList[itemIndex],
-                      videoIndex: itemIndex,
-                      slowMode: slowModeOn,
-                      autoMode: autoModeOn,
-                      loopMode: loopModeOn,
-                    ),
-                  ],
+          child: Stack(
+            children: [
+              CarouselSlider.builder(
+                carouselController: carouselController,
+                options: CarouselOptions(
+                    height: (height / 10) * 7,
+                    viewportFraction: 0.999999, // FIXME dirty hack to achieve pre-loading of previous/next page
+                    autoPlay: false,
+                    enlargeCenterPage: true
                 ),
+                itemCount: videoList.length,
+                itemBuilder: (BuildContext context, int itemIndex) =>
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Container(
+                            child: vocableVisible
+                                ? Container(
+                              child: Text("Video #$itemIndex", style: TextStyle(color: ColorsLectary.white),), padding: EdgeInsets.all(10),)
+                                : IconButton(icon: Icon(Icons.visibility, color: ColorsLectary.green,), iconSize: 60,
+                              onPressed: () => setState(() {
+                                vocableVisible = vocableVisible ? false : true;
+                              }),
+                            )
+                        ),
+                        LectaryVideoPlayer(
+                          videoPath: videoList[itemIndex],
+                          videoIndex: itemIndex,
+                          slowMode: slowModeOn,
+                          autoMode: autoModeOn,
+                          loopMode: loopModeOn,
+                        ),
+                      ],
+                    ),
+              ),
+              ..._buildCarouselNavigationOverlay(),
+            ]
           ),
         ),
         /// Media Control Area
@@ -133,6 +138,51 @@ class _LectureScreenState extends State<LectureScreen> {
         ),
       ],
     );
+  }
+
+  List<Widget> _buildCarouselNavigationOverlay() {
+    return [
+      Align(
+        alignment: Alignment.centerLeft,
+        child: ClipRect(
+          child: Align(
+            widthFactor: 0.5,
+            alignment: Alignment.center,
+            child: Opacity(
+              opacity: 0.3,
+              child: IconButton(
+                padding: EdgeInsets.all(0.0),
+                iconSize: 60,
+                icon: Icon(Icons.keyboard_arrow_left, color: ColorsLectary.white,),
+                onPressed: () => carouselController.previousPage(
+                  duration: Duration(milliseconds: 300), curve: Curves.linear
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.centerRight,
+        child: ClipRect(
+          child: Align(
+            widthFactor: 0.5,
+            alignment: Alignment.center,
+            child: Opacity(
+              opacity: 0.3,
+              child: IconButton(
+                padding: EdgeInsets.all(0.0),
+                iconSize: 60,
+                icon: Icon(Icons.keyboard_arrow_right, color: ColorsLectary.white,),
+                onPressed: () => carouselController.nextPage(
+                    duration: Duration(milliseconds: 300), curve: Curves.linear
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+    ];
   }
 
   Expanded _buildButton(color, icon, int size, {int iconContainerWidth=0, Function func=emptyFunction}) {
