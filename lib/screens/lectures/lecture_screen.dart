6706756b24616e30a7fa60fdc28c4d40e409c7fc
Guenter <guenter.windsperger@gmx.at>
2020-07-06 11:45:34 +0200

    adding mock data and integrating the additional media type viewers into the carousel

--- lib/screens/lectures/lecture_screen.dart
+++ lib/screens/lectures/lecture_screen.dart
@@ -1,11 +1,41 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:lectary/screens/lectures/widgets/picture_viewer.dart';
 import 'package:lectary/screens/lectures/widgets/lectary_text_area.dart';
 import 'package:lectary/screens/lectures/widgets/lectary_video_player.dart';
+import 'package:lectary/screens/lectures/widgets/text_viewer.dart';
 import 'package:lectary/utils/colors.dart';
 import 'package:provider/provider.dart';
+
+abstract class MediaItem {
+  final String text;
+  final String media;
+
+  MediaItem(this.text, this.media);
+}
+
+class VideoItem extends MediaItem {
+  VideoItem({String text, String media}) : super(text, media);
+}
+
+class PictureItem extends MediaItem {
+  PictureItem({String text, String media}) : super(text, media);
+
+}
+
+class TextItem extends MediaItem {
+  TextItem({String text, String media}) : super(text, media);
+}
+
+final mediaList = List<MediaItem>.generate(20, (index) => index % 3 == 0
+    ? VideoItem(text: "Vocable 1", media: 'assets/videos/mock_videos/video1.mp4')
+    : (index % 3 == 1
+    ? PictureItem(text: "Vocable 2", media: 'assets/pictures/mock_pictures/mushroom.jpg')
+    : TextItem(text: "Vocable 3", media: "Vocabulario"))
+);
 
 
 final List<String> videoList = List.generate(1000, (index) => 'assets/videos/mock_videos/video${(index%3)+1}.mp4');
@@ -61,7 +91,7 @@ class _LectureScreenState extends State<LectureScreen> {
                       Provider.of<VideosProvider>(context, listen: false).currentVideo = index;
                     }
                 ),
-                itemCount: videoList.length,
+                itemCount: mediaList.length,
                 itemBuilder: (BuildContext context, int itemIndex) =>
                     /// media area - text area with video player
                     Column(
@@ -69,15 +99,36 @@ class _LectureScreenState extends State<LectureScreen> {
                       children: <Widget>[
                         TextArea(
                           hideVocableModeOn: hideVocableModeOn,
-                          text: itemIndex.toString(),
+                          text: mediaList[itemIndex].text,
                         ),
+                        mediaList[itemIndex] is VideoItem
+                            ?
                         LectaryVideoPlayer(
-                          videoPath: videoList[itemIndex],
+                          videoPath: mediaList[itemIndex].media,
                           videoIndex: itemIndex,
                           slowMode: slowModeOn,
                           autoMode: autoModeOn,
                           loopMode: loopModeOn,
-                        ),
+                        )
+                            :
+                        (mediaList[itemIndex] is PictureItem
+                            ?
+                        PictureViewer(
+                              picturePath: mediaList[itemIndex].media,
+                              pictureIndex: itemIndex,
+                              slowMode: slowModeOn,
+                              autoMode: autoModeOn,
+                              loopMode: loopModeOn,
+                        )
+                            :
+                        TextViewer(
+                          content: mediaList[itemIndex].media,
+                          textIndex: itemIndex,
+                          slowMode: slowModeOn,
+                          autoMode: autoModeOn,
+                          loopMode: loopModeOn,
+                        )
+                        )
                       ],
                     ),
               ),
