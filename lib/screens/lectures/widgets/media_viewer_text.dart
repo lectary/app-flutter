89bbe03ceab1e49e0d5baf8c69f5e82692a5a002
Guenter <guenter.windsperger@gmx.at>
2020-07-07 20:05:24 +0200

    adding slowMode to media viewer text
    - implementing an animation, where the text is smoothly build up by continuously replacing the blank chars of an string with the chars of the text randomly

--- lib/screens/lectures/widgets/media_viewer_text.dart
+++ lib/screens/lectures/widgets/media_viewer_text.dart
@@ -17,25 +17,88 @@ class TextViewer extends StatefulWidget {
   _TextViewerState createState() => _TextViewerState();
 }
 
-class _TextViewerState extends State<TextViewer> {
+class _TextViewerState extends State<TextViewer> with TickerProviderStateMixin {
   bool showText = false;
+
+  AnimationController _animationController;
+  Animation<int> _characterCount;
+
+  String finalContent;
+  int index = -1;
+  List<int> randomBox;
+
+  @override
+  void initState() {
+    super.initState();
+    // placeholder of empty chars that get replaced during the animation
+    finalContent = " " * widget.content.length;
+    // array of indices representing the content, to pick one index randomly
+    randomBox = List.generate(widget.content.length, (_index) => _index);
+
+    _animationController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    _characterCount = StepTween(begin: 0, end: widget.content.length-1).animate(_animationController)
+      ..addListener(() {
+        // update only when index changed
+        if (index < _characterCount.value) {
+          index = _characterCount.value;
+          randomBox.shuffle();
+          int rndIndex = randomBox.removeLast();
+          setState(() {
+            finalContent = finalContent.replaceRange(rndIndex, rndIndex+1, widget.content[rndIndex]);
+          });
+        }
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (!widget.slowMode) {
+      _animationController.reset();
+      finalContent = widget.content;
+    }
     return GestureDetector(
       onTap: () {
         setState(() {
+          if (widget.slowMode) {
+            if (showText) {
+              _resetAnimation();
+            } else {
+              _resetAnimation();
+              _animationController.forward();
+            }
+          }
           showText = showText ? false : true;
         });
       },
       child: Center(
         child: AspectRatio(
-            aspectRatio: 4/3,
+            aspectRatio: 4 / 3,
             child: showText
-                ? Center(child: Text(widget.content, style: TextStyle(fontSize: 28, color: ColorsLectary.white)))
-                : Icon(Icons.subject, size: 120,)
-        ),
+                ? Center(
+                    child: Text(widget.slowMode ? finalContent : widget.content,
+                        style: TextStyle(
+                            fontSize: 28, color: ColorsLectary.white)))
+                : Icon(
+                    Icons.subject,
+                    size: 120,
+                  )),
       ),
     );
+  }
+
+  void _resetAnimation() {
+    _animationController.reset();
+    finalContent = " " * widget.content.length;
+    index = -1;
+    randomBox = List.generate(widget.content.length, (_index) => _index);
   }
 }
