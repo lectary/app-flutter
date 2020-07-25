500a222a6afdad10aa471af69578ef2d9baa2247
Günter <e1302775@student.tuwien.ac.at>
2020-07-25 21:14:20 +0200

    extracting static helper methods for deAsciify and lecture metainfo extraction to Util class

---
+++ lib/utils/utils.dart
@@ -0,0 +1,56 @@
+import 'dart:developer';
+
+class Utils {
+
+  static Map<String, dynamic> extractMetaInformation(String fileName) {
+    Map<String, dynamic> result = Map();
+
+    String fileWithoutType = fileName.split(".zip")[0];
+    if (!fileWithoutType.contains("PACK") || !fileWithoutType.contains("LESSON") || !fileWithoutType.contains("LANG")) {
+      // throw new Exception("File has not mandatory meta information!") //FIXME ignore temp due to miss-formatted test cases
+      // TODO add mechanic to send error message to server for informing about wrong packages?
+      log("File has not mandatory meta information! File: " + fileWithoutType);
+    }
+
+    List<String> metaInfos = fileWithoutType.split("---");
+    for (String metaInfo in metaInfos) {
+      String metaInfoType = metaInfo.split("--")[0];
+      String metaInfoValue = metaInfo.split("--")[1];
+
+      switch(metaInfoType) {
+        case "PACK":
+          result.putIfAbsent("PACK", () => _deAsciify(metaInfoValue));
+          break;
+        case "LESSON":
+          result.putIfAbsent("LESSON", () => _deAsciify(metaInfoValue));
+          break;
+        case "LANG":
+          result.putIfAbsent("LANG", () => metaInfoValue);
+          break;
+        case "AUDIO":
+          result.putIfAbsent("AUDIO", () => metaInfoValue);
+          break;
+        case "DATE":
+          result.putIfAbsent("DATE", () => DateTime.parse(metaInfoValue));
+          break;
+        case "SORT":
+          result.putIfAbsent("SORT", () => int.parse(metaInfoValue));
+          break;
+      }
+    }
+    return result;
+  }
+
+
+  static String _deAsciify(String asciifiedString) {
+    String result = asciifiedString;
+
+    // TODO add all cases
+    result = result.replaceAll("_-", "-");
+    result = result.replaceAll("__", " ");
+    result = result.replaceAll("_VS", "/");
+    result = result.replaceAll("_ae", "ä");
+
+    return result;
+  }
+}
