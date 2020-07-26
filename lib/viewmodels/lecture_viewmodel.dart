5adf4a701e27acd8f7b3e6b3d4b758b444292ed0
Günter <e1302775@student.tuwien.ac.at>
2020-07-26 11:41:11 +0200

    refactoring viewmodel
    - cleaning code
    - adding repo and retrieving persisted lectures as second list (stream)
    - adding transform method to group lectures by pack

--- lib/viewmodels/lecture_viewmodel.dart
+++ lib/viewmodels/lecture_viewmodel.dart
@@ -1,34 +1,50 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:lectary/models/lecture.dart';
-import 'package:lectary/repositories/lecture_repository.dart';
+import 'package:lectary/data/entities/lecture.dart';
+import 'package:lectary/data/repositories/lecture_repository.dart';
+import 'package:lectary/models/lecture_package.dart';
+
+import 'package:collection/collection.dart';
 
 enum Status { loading, error, completed }
 
 class LectureViewModel with ChangeNotifier {
-  LectureRepository _lectureRepository = LectureRepository();
+  final LectureRepository _lectureRepository;
 
-  List<Lecture> _lectureList = List();
+  List<Lecture> _availableLectures = List();
   Status _status = Status.completed;
   String _message;
 
-  List<Lecture> get lectureList => _lectureList;
+  List<Lecture> get availableLectures => _availableLectures;
   Status get status => _status;
   String get message => _message;
+
+  Stream<List<LecturePackage>> localLectures;
+
+  LectureViewModel({@required lectureRepository})
+      : _lectureRepository = lectureRepository
+  {
+    localLectures = _lectureRepository.watchAllLectures()
+        .map((list) => groupLecturesByPack(list));
+  }
+
+  List<LecturePackage> groupLecturesByPack(List<Lecture> lectureList) {
+    final lecturesByPack = groupBy(lectureList, (lecture) => (lecture as Lecture).pack);
+    List<LecturePackage> packList = List();
+    lecturesByPack.forEach((key, value) => packList.add(LecturePackage(key, value)));
+    return packList;
+  }
+
+  /*@override
+  void dispose() {
+    _lectureRepository.dispose();
+    super.dispose();
+  }*/
 
   Future<void> loadLectures() async {
     _status = Status.loading;
     notifyListeners();
-
-    // TODO refactor to fetch in parallel
-//    final lectureLists = await Future.wait([
-//      loadLectures(),
-//      loadLecturesFromLocalDB()
-//    ]);
-//
-//    final lecturesFromServer = lectureLists[0];
-//    final lecturesFromLocalDB = lectureLists[1];
 
     try {
       List<Lecture> remoteList = await _lectureRepository.loadLecturesRemote();
@@ -36,7 +52,7 @@ class LectureViewModel with ChangeNotifier {
       List<Lecture> localList = await _lectureRepository.loadLecturesLocal();
       log("loaded local lectures");
 
-      _lectureList = _mergeLectureLists(remoteList, localList);
+      _availableLectures = _mergeLectureLists(remoteList, localList);
 
       _status = Status.completed;
       notifyListeners();
@@ -45,7 +61,6 @@ class LectureViewModel with ChangeNotifier {
       _message = e.toString();
       notifyListeners();
     }
-
   }
 
   List<Lecture> _mergeLectureLists(List<Lecture> remoteList, List<Lecture> localList) {
@@ -54,7 +69,7 @@ class LectureViewModel with ChangeNotifier {
     resultList.forEach((remote) {
       localList.forEach((local) {
         if (remote.lesson == local.lesson) {
-          if (remote.date.isBefore(local.date)) {
+          if (DateTime.parse(remote.date).isBefore(DateTime.parse(local.date))) {
             remote.lectureStatus = LectureStatus.updateAvailable;
           } else {
             remote.lectureStatus = LectureStatus.persisted;
@@ -76,7 +91,7 @@ class LectureViewModel with ChangeNotifier {
   }
 
   Future<void> loadSingleLectureFromServer(int lectureIndex) async {
-    _lectureList[lectureIndex].lectureStatus = LectureStatus.downloading;
+    _availableLectures[lectureIndex].lectureStatus = LectureStatus.downloading;
     notifyListeners();
 
     // TODO unzip
