07180a8f991d04f63cc53bc46014d9f4cd800830
Günter <e1302775@student.tuwien.ac.at>
2020-07-26 11:27:17 +0200

    adding database provider class for managing db instance and handling database stuff

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -7,10 +7,74 @@ import 'dao/lecture_dao.dart';
 import 'entities/lecture.dart';
 import 'entities/vocable.dart';
 
-
 part 'database.g.dart'; // the generated code will be there
 
 @Database(version: 1, entities: [Lecture, Vocable])
 abstract class LectureDatabase extends FloorDatabase {
   LectureDao get lectureDao;
+}
+
+class DatabaseProvider {
+  static final _instance = DatabaseProvider._internal();
+  static DatabaseProvider instance = _instance;
+
+  DatabaseProvider._internal();
+
+  static LectureDatabase _db;
+
+  Future<LectureDatabase> get db async {
+    if (_db == null) {
+      _db = await $FloorLectureDatabase.databaseBuilder('lectures.db')
+          .addCallback(callback)
+          .build();
+    }
+    return _db;
+  }
+
+  Future<void> closeDB() async {
+    if (_db != null) {
+      _db.close();
+    }
+  }
+
+  /// insert mock data
+  final callback = Callback(onOpen: (database) {
+    database.delete("lectures");
+    final lectures = List<Map<String, dynamic>>.of({
+      {
+        "file_name":
+            "PACK--Alpen__Adria__Universit_aet---LESSON--AAU__Lektion__4---LANG--OGS-DE---SORT--104---DATE--2019-04-29.zip",
+        "file_size": 5,
+        "vocable_count": 5,
+        "pack": "Alpen Adria Universität",
+        "lesson": "AAU Lektion 4",
+        "date": "2019-04-29",
+        "lang": "test"
+      },
+      {
+        "file_name":
+            "PACK--Alpen__Adria__Universit_aet---LESSON--AAU__Lektion__5---LANG--OGS-DE---SORT--105---DATE--2019-04-29.zip",
+        "file_size": 5,
+        "vocable_count": 5,
+        "pack": "Alpen Adria Universität",
+        "lesson": "AAU Lektion 5",
+        "date": "2019-04-30",
+        "lang": "test"
+      },
+      {
+        "file_name":
+            "PACK--Alpen__Adria__Universit_aet---LESSON--TEST---LANG--OGS-DE---SORT--105---DATE--2019-04-29.zip",
+        "file_size": 5,
+        "vocable_count": 5,
+        "pack": "Alpen Adria Universität",
+        "lesson": "TEST",
+        "date": "2019-04-29",
+        "lang": "test"
+      },
+    });
+
+    lectures.forEach((lecture) {
+      database.insert("lectures", lecture);
+    });
+  });
 }
