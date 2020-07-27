02c90a64fe4ef32ed5f5157128b84b9a8387e710
Günter <e1302775@student.tuwien.ac.at>
2020-07-27 14:29:35 +0200

    adding vocable dao methods to repository

--- lib/data/repositories/lecture_repository.dart
+++ lib/data/repositories/lecture_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lectary/data/database.dart';
 import 'package:lectary/data/entities/lecture.dart';
+import 'package:lectary/data/entities/vocable.dart';
 import 'package:lectary/services/lectary_api.dart';
 
 /// Repository class for encapsulating data access independent of the source
@@ -34,6 +35,34 @@ class LectureRepository {
 
   Future<void> deleteLecture(Lecture lecture) {
     return _lectureDatabase.lectureDao.deleteLecture(lecture);
+  }
+
+  Future<List<Vocable>> findAllVocables() {
+    return _lectureDatabase.vocableDao.findAllVocables();
+  }
+
+  Future<List<Vocable>> findVocablesByLectureId(int lectureId) {
+    return _lectureDatabase.vocableDao.findVocablesByLectureId(lectureId);
+  }
+
+  Future<List<Vocable>> findVocablesByLecturePack(String lecturePack) {
+    return _lectureDatabase.vocableDao.findVocablesByLecturePack(lecturePack);
+  }
+
+  Future<List<int>> insertVocables(List<Vocable> vocables) {
+    return _lectureDatabase.vocableDao.insertVocables(vocables);
+  }
+
+  Future<void> updateVocable(Vocable vocable) {
+    return _lectureDatabase.vocableDao.updateVocable(vocable);
+  }
+
+  Future<void> deleteVocablesByLectureId(int lectureId) {
+    return _lectureDatabase.vocableDao.deleteVocablesByLectureId(lectureId);
+  }
+
+  Future<void> deleteAllVocables() {
+    return _lectureDatabase.vocableDao.deleteAllVocables();
   }
 
   Future<void> dispose() async {
