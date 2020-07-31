90992e9fb1fc21f7442da0a3323ce2d09242c0bb
Günter <e1302775@student.tuwien.ac.at>
2020-07-31 18:21:17 +0200

    adding various tests for all util methods

--- test/util_test.dart
+++ test/util_test.dart
@@ -1,12 +1,98 @@
-import 'dart:developer';
-
+import 'package:lectary/models/media_type_enum.dart';
+import 'package:lectary/utils/exceptions/media_type_exception.dart';
 import 'package:lectary/utils/utils.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('currentDate should print date in format yyyy-MM-dd', () {
+void main() async {
+  group('extracting filename of filepath |', () {
+    test('Test1 - successful extraction with slash as path separator', () {
+      String zipFile = "test1.com/test2/fileName.zip";
+      String dirName = Utils.extractFileName(zipFile);
+      expect(dirName, "fileName");
+    });
+
+    test('Test2 - successful extraction with backslash as path separator ', () {
+      String zipFile = "test1.com\\test2\\fileName.zip";
+      String dirName = Utils.extractFileName(zipFile);
+      expect(dirName, "fileName");
+    });
+
+    test('Test3 - successful extraction with slash and backslash as path separator', () {
+      String zipFile = "test1.com\\test2/fileName.zip";
+      String dirName = Utils.extractFileName(zipFile);
+      expect(dirName, "fileName");
+    });
+
+    test('Test4 - filename without extension', () {
+      String filename = "withoutExtension";
+      String extractedName = Utils.extractFileName(filename);
+      expect(extractedName, "withoutExtension");
+    });
+
+    test('Test5 - empty string should return empty string', () {
+      String filename = "";
+      String extractedName = Utils.extractFileName(filename);
+      expect(extractedName, "");
+    });
+  });
+
+  group('extracting directory name of filepath |', () {
+    test('Test1 - successful extraction with slash as path separator', () {
+      String fileName = "/test.com/dirName/file.mp4";
+      String dirName = Utils.extractDirName(fileName);
+      expect(dirName, "dirName");
+    });
+
+    test('Test2 - successful extraction with backslash as path separator', () {
+      String fileName = "\\test.com\\dirName\\file.mp4";
+      String dirName = Utils.extractDirName(fileName);
+      expect(dirName, "dirName");
+    });
+
+    test('Test3 - successful extraction with slash and backslash as path separator', () {
+      String fileName = "\\test.com/dirName\\file.mp4";
+      String dirName = Utils.extractDirName(fileName);
+      expect(dirName, "dirName");
+    });
+
+    test('Test4 - filename without extension', () {
+      String filename = "withoutDirectory";
+      String extractedDirName = Utils.extractDirName(filename);
+      expect(extractedDirName, "");
+    });
+
+    test('Test5 - empty string should return empty string', () {
+      String filename = "";
+      String extractedDirName = Utils.extractDirName(filename);
+      expect(extractedDirName, "");
+    });
+  });
+
+  test('file extension should get extracted out of full filename', () {
+    String fileName = "test1.com/test2/file.mp4";
+    String extension = Utils.extractFileExtension(fileName);
+    expect(extension, "mp4");
+  });
+
+  group('MediaType conversion |', () {
+    test('should success with mp4 png jpg or txt and should work case insensitive', () {
+      MediaType type1 = MediaType.fromString("mp4");
+      expect(type1, MediaType.MP4);
+      MediaType type2 = MediaType.fromString("png");
+      expect(type2, MediaType.PNG);
+      MediaType type3 = MediaType.fromString("JPG");
+      expect(type3, MediaType.JPG);
+      MediaType type4 = MediaType.fromString("TXT");
+      expect(type4, MediaType.TXT);
+    });
+
+    test('should throw an exception on unknown extension/type', () {
+      expect(() => MediaType.fromString("UNKNOWN"), throwsA(TypeMatcher<MediaTypeException>()));
+    });
+  });
+
+  test('method currentDate should print current date in ISO-8601 format yyyy-MM-dd', () {
     String currentDate = Utils.currentDate();
-    print("Current date: " + currentDate);
 
     expect(currentDate, ""
         "${DateTime.now().year}"
@@ -14,7 +100,7 @@ void main() {
         "-${DateTime.now().day < 10 ? "0" + DateTime.now().day.toString() : DateTime.now().day}");
   });
 
-  test('method should fill string with leading zeros up to length of 5', () {
+  test('method fillWithLeadingZeros should fill string with leading zeros up to a length of 5', () {
     String sort1 = "1";
     String sort2 = "01";
     String sort3 = "001";
