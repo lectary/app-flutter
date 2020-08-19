140c99687392739646fcbec2d5ac0cd691b44a25
Günter <e1302775@student.tuwien.ac.at>
2020-08-20 00:36:37 +0200

    adding test case search results
    - recreating minimal app skeleton with materialApp, localization and needed provider-mock and fake-button-structure for opening widget that should be tested as new MaterialRoute

---
+++ test/duplicates_in_search_test.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:lectary/data/db/entities/lecture.dart';
+import 'package:lectary/data/db/entities/vocable.dart';
+import 'package:lectary/data/repositories/lecture_repository.dart';
+import 'package:lectary/i18n/localizations.dart';
+import 'package:lectary/screens/lectures/vocable_search_screen.dart';
+import 'package:lectary/viewmodels/carousel_viewmodel.dart';
+
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+class MockLectureRepository extends Mock implements LectureRepository {}
+
+void main() async {
+
+  group('Testing elements of search result screen |', () {
+    Stream lectureStream = StreamController<List<Lecture>>().stream;
+
+    List<Vocable> vocablesWithDuplicates = List.of({
+      Vocable(id: 1, lectureId: 1, vocable: "Haus", mediaType: "PNG"),
+      Vocable(id: 2, lectureId: 1, vocable: "Haus", mediaType: "TXT"),
+      Vocable(id: 3, lectureId: 2, vocable: "Haus", mediaType: "MP4"),
+      Vocable(id: 4, lectureId: 3, vocable: "Baum", mediaType: "TXT")
+    });
+
+    testWidgets('Test1 - testing correct appearance with vocable duplicates', (WidgetTester tester) async {
+      final mockRepo = MockLectureRepository();
+      when(mockRepo.findAllVocables()).thenAnswer((_) async => Future.value(vocablesWithDuplicates));
+      when(mockRepo.watchAllLectures()).thenAnswer((_) => lectureStream);
+
+      CarouselViewModel carouselViewModel = CarouselViewModel(lectureRepository: mockRepo);
+      await carouselViewModel.loadAllVocables();
+
+      final key = GlobalKey<NavigatorState>();
+      await tester.pumpWidget(
+        ChangeNotifierProvider.value(
+          value: carouselViewModel,
+          child: MaterialApp(
+            navigatorKey: key,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              // following localizations are needed!
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate
+            ],
+            supportedLocales: [
+              const Locale('de', 'DE'),
+            ],
+            title: 'Flutter Test Wrapper',
+            home: FlatButton(
+              onPressed: () => key.currentState.push(
+                MaterialPageRoute<void>(
+                    settings: RouteSettings(
+                        arguments:
+                            VocableSearchScreenArguments(openSearch: false)),
+                    builder: (_) => VocableSearchScreen()),
+              ), child: const SizedBox(),
+            ),
+          ),
+        ),
+      );
+      await tester.tap(find.byType(FlatButton));
+      await tester.pumpAndSettle();
+
+      // assert title
+      final titleFinder = find.text('Alle Vokabel');
+      expect(titleFinder, findsOneWidget);
+
+      // assert number of duplicates
+      final listElementFinder1 = find.text("Haus");
+      expect(listElementFinder1, findsNWidgets(3));
+
+      // assert number of non-duplicates
+      final listElementFinder2 = find.text("Baum");
+      expect(listElementFinder2, findsOneWidget);
+
+      // assert icons
+      final listElementIconFinder1 = find.byIcon(Icons.movie);
+      expect(listElementIconFinder1, findsOneWidget);
+      final listElementIconFinder2 = find.byIcon(Icons.insert_photo);
+      expect(listElementIconFinder2, findsOneWidget);
+      final listElementIconFinder3 = find.byIcon(Icons.subject);
+      expect(listElementIconFinder3, findsOneWidget);
+    });
+  });
+}
