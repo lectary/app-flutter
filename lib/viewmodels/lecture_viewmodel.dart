lectary/app-flutter

commit c3fcb1c4512f339f9f4a881bbd068c07cbb3c0c9

incorporating new sort method and adding an extra field for sorting by name to lecture and vocable to sort via database

lib/viewmodels/lecture_viewmodel.dart
@@ -65,13 +65,14 @@ class LectureViewModel with ChangeNotifier {
       lecturesWithSortMeta.sort((l1, l2) => l1.sort.compareTo(l2.sort));
       // 2) sort lessons without SORT-meta info lexicographic by lesson
       List<Lecture> lecturesWithoutSortMeta = mergedLectureList.where((lecture) => lecture.sort == null).toList();
-      lecturesWithoutSortMeta.sort((l1, l2) => l1.lesson.toLowerCase().compareTo(l2.lesson.toLowerCase()));
+      //lecturesWithoutSortMeta.sort((l1, l2) => l1.lesson.toLowerCase().compareTo(l2.lesson.toLowerCase()));
+      lecturesWithoutSortMeta.sort((l1, l2) => Utils.customCompareTo(l1.lesson, l2.lesson));
       // merge both sorted lists and group by lecture pack
       List<Lecture> allLectures = lecturesWithSortMeta;
       allLectures.addAll(lecturesWithoutSortMeta);
       List<LecturePackage> groupedLectureList = _groupLecturesByPack(allLectures);
       // 3) sort lexicographic by packs
-      groupedLectureList.sort((p1, p2) => p1.title.toLowerCase().compareTo(p2.title.toLowerCase()));
+      groupedLectureList.sort((p1, p2) => Utils.customCompareTo(p1.title, p2.title));
 
       _availableLectures = groupedLectureList;
       log("loaded lectures");
@@ -193,11 +194,12 @@ class LectureViewModel with ChangeNotifier {
       int newId = await _lectureRepository.insertLecture(lecture); // persist lecture and retrieve autoGenerated id
       vocables.forEach((element) {
         element.lectureId = newId;
+        String deAsciified = Utils.deAsciify(element.vocable);
         codingEntries == null
-        ? element.vocable = Utils.deAsciify(element.vocable)
-        : element.vocable = Utils.deAsciify(element.vocable, codingEntries: codingEntries);
+            ? element.vocable = deAsciified
+            : element.vocable = Utils.deAsciify(element.vocable, codingEntries: codingEntries);
+        element.vocableSort = Utils.replaceForSort(deAsciified);
       }); // set id in every vocable and deAsciify
-      vocables.forEach((voc) { log(voc.toString()); });
       await _lectureRepository.insertVocables(vocables); // save the vocables in the database
 
       _availableLectures[indexPack].children[indexLecture].id = newId; // set newId in the lecture list for later usage like updating or deleting
@@ -260,10 +262,11 @@ class LectureViewModel with ChangeNotifier {
       log("saving new lecture: ${lecture.toString()}");
       int newId = await _lectureRepository.insertLecture(lecture); // persist lecture and retrieve autoGenerated id
       vocables.forEach((element) {
-        element.lectureId = newId;
+        String deAsciified = Utils.deAsciify(element.vocable);
         codingEntries == null
-            ? element.vocable = Utils.deAsciify(element.vocable)
+            ? element.vocable = deAsciified
             : element.vocable = Utils.deAsciify(element.vocable, codingEntries: codingEntries);
+        element.vocableSort = Utils.replaceForSort(deAsciified);
       }); // set id in every vocable and deAsciify
       await _lectureRepository.insertVocables(vocables); // save the vocables in the database
 

lib/data/db/database.g.dart
@@ -86,9 +86,9 @@ class _$LectureDatabase extends LectureDatabase {
       },
       onCreate: (database, version) async {
         await database.execute(
-            'CREATE TABLE IF NOT EXISTS `lectures` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `file_name` TEXT NOT NULL, `file_size` INTEGER NOT NULL, `vocable_count` INTEGER NOT NULL, `pack` TEXT NOT NULL, `lesson` TEXT NOT NULL, `lang_media` TEXT NOT NULL, `lang_vocable` TEXT NOT NULL, `audio` TEXT, `date` TEXT NOT NULL, `sort` TEXT)');
+            'CREATE TABLE IF NOT EXISTS `lectures` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `file_name` TEXT NOT NULL, `file_size` INTEGER NOT NULL, `vocable_count` INTEGER NOT NULL, `pack` TEXT NOT NULL, `lesson` TEXT NOT NULL, `lesson_sort` TEXT NOT NULL, `lang_media` TEXT NOT NULL, `lang_vocable` TEXT NOT NULL, `audio` TEXT, `date` TEXT NOT NULL, `sort` TEXT)');
         await database.execute(
-            'CREATE TABLE IF NOT EXISTS `vocables` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `lecture_id` INTEGER NOT NULL, `vocable` TEXT NOT NULL, `media_type` TEXT NOT NULL, `media` TEXT NOT NULL, `vocable_progress` INTEGER NOT NULL, FOREIGN KEY (`lecture_id`) REFERENCES `lectures` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION)');
+            'CREATE TABLE IF NOT EXISTS `vocables` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `lecture_id` INTEGER NOT NULL, `vocable` TEXT NOT NULL, `vocable_sort` TEXT NOT NULL, `media_type` TEXT NOT NULL, `media` TEXT NOT NULL, `vocable_progress` INTEGER NOT NULL, FOREIGN KEY (`lecture_id`) REFERENCES `lectures` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION)');
         await database.execute(
             'CREATE TABLE IF NOT EXISTS `abstracts` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `file_name` TEXT NOT NULL, `pack` TEXT NOT NULL, `text` TEXT NOT NULL, `date` TEXT NOT NULL)');
         await database.execute(
@@ -136,6 +136,7 @@ class _$LectureDao extends LectureDao {
                   'vocable_count': item.vocableCount,
                   'pack': item.pack,
                   'lesson': item.lesson,
+                  'lesson_sort': item.lessonSort,
                   'lang_media': item.langMedia,
                   'lang_vocable': item.langVocable,
                   'audio': item.audio,
@@ -154,6 +155,7 @@ class _$LectureDao extends LectureDao {
                   'vocable_count': item.vocableCount,
                   'pack': item.pack,
                   'lesson': item.lesson,
+                  'lesson_sort': item.lessonSort,
                   'lang_media': item.langMedia,
                   'lang_vocable': item.langVocable,
                   'audio': item.audio,
@@ -172,6 +174,7 @@ class _$LectureDao extends LectureDao {
                   'vocable_count': item.vocableCount,
                   'pack': item.pack,
                   'lesson': item.lesson,
+                  'lesson_sort': item.lessonSort,
                   'lang_media': item.langMedia,
                   'lang_vocable': item.langVocable,
                   'audio': item.audio,
@@ -193,6 +196,7 @@ class _$LectureDao extends LectureDao {
       vocableCount: row['vocable_count'] as int,
       pack: row['pack'] as String,
       lesson: row['lesson'] as String,
+      lessonSort: row['lesson_sort'] as String,
       langMedia: row['lang_media'] as String,
       langVocable: row['lang_vocable'] as String,
       audio: row['audio'] as String,
@@ -257,6 +261,7 @@ class _$VocableDao extends VocableDao {
                   'id': item.id,
                   'lecture_id': item.lectureId,
                   'vocable': item.vocable,
+                  'vocable_sort': item.vocableSort,
                   'media_type': item.mediaType,
                   'media': item.media,
                   'vocable_progress': item.vocableProgress
@@ -269,6 +274,7 @@ class _$VocableDao extends VocableDao {
                   'id': item.id,
                   'lecture_id': item.lectureId,
                   'vocable': item.vocable,
+                  'vocable_sort': item.vocableSort,
                   'media_type': item.mediaType,
                   'media': item.media,
                   'vocable_progress': item.vocableProgress
@@ -284,6 +290,7 @@ class _$VocableDao extends VocableDao {
       id: row['id'] as int,
       lectureId: row['lecture_id'] as int,
       vocable: row['vocable'] as String,
+      vocableSort: row['vocable_sort'] as String,
       mediaType: row['media_type'] as String,
       media: row['media'] as String,
       vocableProgress: row['vocable_progress'] as int);

lib/data/db/entities/lecture.dart
@@ -36,6 +36,10 @@ class Lecture {
   @ColumnInfo(nullable: false)
   String lesson;
 
+  // used for sorting
+  @ColumnInfo(name: "lesson_sort", nullable: false)
+  String lessonSort;
+
   @ColumnInfo(name: "lang_media", nullable: false)
   String langMedia;
 
@@ -56,6 +60,7 @@ class Lecture {
       @required this.vocableCount,
       @required this.pack,
       @required this.lesson,
+      @required this.lessonSort,
       @required this.langMedia,
       @required this.langVocable,
       this.audio,
@@ -72,6 +77,7 @@ class Lecture {
     this.vocableCount = lecture.vocableCount;
     this.pack = lecture.pack;
     this.lesson = lecture.lesson;
+    this.lessonSort = lecture.lessonSort;
     this.langMedia = lecture.langMedia;
     this.langVocable = lecture.langVocable;
     this.audio = lecture.audio;
@@ -97,6 +103,7 @@ class Lecture {
       vocableCount: json['vocableCount'],
       pack: metaInfo.remove("PACK"),
       lesson: metaInfo.remove("LESSON"),
+      lessonSort: metaInfo.remove("LESSON-SORT"),
       langMedia: metaInfo.remove("LANG-MEDIA"),
       langVocable: metaInfo.remove("LANG-VOCABLE"),
       audio: metaInfo.containsKey("AUDIO") ? metaInfo.remove("AUDIO") : null,
@@ -107,6 +114,6 @@ class Lecture {
 
   @override
   String toString() {
-    return 'Lecture{id: $id, lectureStatus: $lectureStatus, fileNameUpdate: $fileNameUpdate, fileName: $fileName, fileSize: $fileSize, vocableCount: $vocableCount, pack: $pack, lesson: $lesson, langMedia: $langMedia, langVocable: $langVocable, audio: $audio, date: $date, sort: $sort}';
+    return 'Lecture{id: $id, lectureStatus: $lectureStatus, fileNameUpdate: $fileNameUpdate, fileName: $fileName, fileSize: $fileSize, vocableCount: $vocableCount, pack: $pack, lesson: $lesson, lessonSort: $lessonSort, langMedia: $langMedia, langVocable: $langVocable, audio: $audio, date: $date, sort: $sort}';
   }
 }

lib/data/db/entities/vocable.dart
@@ -23,6 +23,10 @@ class Vocable {
   @ColumnInfo(nullable: false)
   String vocable;
 
+  // used for sorting
+  @ColumnInfo(name: "vocable_sort", nullable: false)
+  String vocableSort;
+
   @ColumnInfo(name: "media_type", nullable: false)
   String mediaType;
 
@@ -36,12 +40,13 @@ class Vocable {
       {this.id,
       @required this.lectureId,
       @required this.vocable,
+      @required this.vocableSort,
       @required this.mediaType,
       @required this.media,
       @required this.vocableProgress});
 
   @override
   String toString() {
-    return 'Vocable{id: $id, lectureId: $lectureId, vocable: $vocable, mediaType: $mediaType, media: $media, vocableProgress: $vocableProgress}';
+    return 'Vocable{id: $id, lectureId: $lectureId, vocable: $vocable, vocableSort: $vocableSort, mediaType: $mediaType, media: $media, vocableProgress: $vocableProgress}';
   }
 }

commit 3d042eff74a4d631dbdd751a9138cb04644e7cf6

[BUG21]: Fixing missing lectureId in vocable entity on lecture update and code cleaning
- fixing bug introduced with commit c3fcb1c4512f339f9f4a881bbd068c07cbb3c0c9, by readding the setting of the lectureId in the updated vocable list
- removing deprecated filter constraint

lib/viewmodels/lecture_viewmodel.dart
@@ -175,9 +175,7 @@ class LectureViewModel with ChangeNotifier {
       }
     });
 
-    if (_settingViewModel.settingLearningLanguage != "ALLE") {
-      resultList = resultList.where((lecture) => lecture.langMedia == _settingViewModel.settingLearningLanguage).toList();
-    }
+    resultList = resultList.where((lecture) => lecture.langMedia == _settingViewModel.settingLearningLanguage).toList();
 
     return resultList;
   }
@@ -281,6 +279,7 @@ class LectureViewModel with ChangeNotifier {
       log("saving new lecture: ${lecture.toString()}");
       int newId = await _lectureRepository.insertLecture(lecture); // persist lecture and retrieve autoGenerated id
       vocables.forEach((element) {
+        element.lectureId = newId;
         String deAsciified = Utils.deAsciify(element.vocable);
         codingEntries == null
             ? element.vocable = deAsciified