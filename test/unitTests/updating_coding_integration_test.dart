9a6650d2db113482d57f54fc3a7fe9ce9f0b3c18
Günter Windsperger <e1302775@student.tuwien.ac.at>
2021-04-24 23:20:37 +0200

    Remove unused code from coding integration test

--- test/unitTests/updating_coding_integration_test.dart
+++ test/unitTests/updating_coding_integration_test.dart
@@ -13,41 +13,6 @@ import '../shared_mocks.mocks.dart';
 
 @GenerateMocks([LectureRepository])
 void main() async {
-  List<dynamic> json = [
-    {"char": "Á", "asciify": "_CZXA"},
-    {"char": "á", "asciify": "_CZXa"},
-    {"char": "Č", "asciify": "_CZC"},
-    {"char": "č", "asciify": "_CZc"},
-    {"char": "Ď", "asciify": "_CZD"},
-    {"char": "ď", "asciify": "_CZd"},
-    {"char": "É", "asciify": "_CZXE"},
-    {"char": "é", "asciify": "_CZXe"},
-    {"char": "Ě", "asciify": "_CZE"},
-    {"char": "é", "asciify": "_CZe"},
-    {"char": "Í", "asciify": "_CZXI"},
-    {"char": "í", "asciify": "_CZXi"},
-    {"char": "Ň", "asciify": "_CZN"},
-    {"char": "ň", "asciify": "_CZn"},
-    {"char": "Ó", "asciify": "_CZXO"},
-    {"char": "ó", "asciify": "_CZXo"},
-    {"char": "Ř", "asciify": "_CZR"},
-    {"char": "ř", "asciify": "_CZr"},
-    {"char": "Š", "asciify": "_CZS"},
-    {"char": "š", "asciify": "_CZs"},
-    {"char": "Ť", "asciify": "_CZT"},
-    {"char": "ť", "asciify": "_CZt"},
-    {"char": "Ú", "asciify": "_CZXU"},
-    {"char": "ú", "asciify": "_CZXu"},
-    {"char": "Ů", "asciify": "_CZU"},
-    {"char": "ů", "asciify": "_CZu"},
-    {"char": "Ý", "asciify": "_CZXY"},
-    {"char": "ý", "asciify": "_CZXy"},
-    {"char": "Ž", "asciify": "_CZZ"},
-    {"char": "ž", "asciify": "_CZz"}
-  ];
-  List<CodingEntry> newCodingEntries =
-      json.map((entry) => CodingEntry(char: entry["char"], ascii: entry["asciify"])).toList();
-
   Coding coding = Coding(id: 1, fileName: "CODING--CZ---DATE--2020-05-26.json", lang: "CZ", date: "2020-05-26");
   coding.codingStatus = CodingStatus.updateAvailable;
   coding.fileNameUpdate = "CODING--CZ---DATE--2020-05-27.json";
